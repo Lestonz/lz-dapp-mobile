5bd721b8204e986d6fada016acf5bed8785e99be
Melih Şahin <lestonz@hotmail.com>
2022-11-23 05:52:24 +0300

    lz-daap 1.0.1

---
+++ src/cli/index.ts
@@ -0,0 +1,98 @@
+#!/usr/bin/env node
+
+import chalk from 'chalk';
+import prompts from 'prompts';
+
+import { create } from '../main';
+import { CreationStatus } from '../types';
+
+const defaultPadding = 5;
+
+const line = (message: string, padding: number = defaultPadding) =>
+  console.log([...Array(padding)].map(() => ' ').join(''), message);
+
+const gfx = `
+
+ /$$       /$$$$$$$$  /$$$$$$  /$$$$$$$$/$$$$$$  /$$   /$$ /$$$$$$$$
+| $$      | $$_____/ /$$__  $$|__  $$__/$$__  $$| $$$ | $$|_____ $$ 
+| $$      | $$      | $$  |__/   | $$ | $$  | $$| $$$$| $$     /$$/ 
+| $$      | $$$$$   |  $$$$$$    | $$ | $$  | $$| $$ $$ $$    /$$/  
+| $$      | $$__/    |____  $$   | $$ | $$  | $$| $$  $$$$   /$$/   
+| $$      | $$       /$$  | $$   | $$ | $$  | $$| $$|  $$$  /$$/    
+| $$$$$$$$| $$$$$$$$|  $$$$$$/   | $$ |  $$$$$$/| $$ |  $$ /$$$$$$$$
+|________/|________/ |______/    |__/  |______/ |__/  |__/|________/
+
+
+`.trim();
+
+
+function validateUriScheme(value: string): boolean {
+  return /^[a-z0-9]+$/.test(value);
+}
+
+(async () => {
+  console.clear();
+  console.log();
+  gfx.split('\n').map((e) => line(e, 1));
+  console.log();
+
+  line(chalk.green.bold`Hello developer,`);
+  line(chalk.green.bold`Lestonz is here, don't worry!`);
+  line(chalk.green.bold`Visit to lestonz.com`); 
+  console.log();
+  line(chalk.red.bgWhite.bold`Don't forget to follow me on Social Media :)`);
+  console.log();
+  line(chalk.green.bold`Let's Start react-native-lz-dapp!`);
+
+
+  console.log();
+
+  const {
+    name,
+    uriScheme,
+  } = await prompts([
+    {
+      type: 'text',
+      name: 'name',
+      message: 'What is your lz-dapp name?',
+      initial: 'react-native-lz-dapp',
+      validate: (value) => {
+        if (typeof value !== 'string') {
+          return `Expected string, encountered ${typeof value}.`;
+        } else if (!value.length) {
+          return 'Name cannot be empty.';
+        } else if (!value.match(/^[a-z0-9-]+$/i)) {
+          return 'Name must be alphanumeric and contain no special characters other than a hyphen.';
+        } else if (/^\d/.test(value)) {
+          return 'Name cannot begin with a number.';
+        } else if (/^-/.test(value)) {
+          return 'Name cannot begin with a hyphen.';
+        }
+        return true;
+      },
+    },
+    {
+      type: 'text',
+      name: 'uriScheme',
+      message: 'What is the URI scheme?',
+      initial: 'reactnative',
+      validate: (value) => {
+        if (!validateUriScheme(value)) {
+          return `Only lowercase alphanumeric characters are allowed.`;
+        }
+        return true;
+      },
+    },
+  ]);
+
+  const { status, message } = await create({
+    name,
+    uriScheme,
+  });
+
+  if (status === CreationStatus.FAILURE) {
+    throw new Error(message);
+  }
+  console.log(message);
+  console.log();
+})();
---
+++ src/types/index.ts
@@ -0,0 +1,41 @@
+export enum  CreationStatus {
+    SUCCESS,
+    FAILURE,
+}
+
+export type createParams = {
+    readonly name: string;
+    readonly uriScheme: string;
+  };
+  
+  export type HardhatAccount = {
+    readonly privateKey: string;
+    readonly balance: string;
+  };
+  
+  export type HardhatOptions = {
+    readonly hardhat: string;
+    readonly hardhatConfig: string;
+    readonly hardhatAccounts: readonly HardhatAccount[];
+  };
+  
+  export type createContextOptions = createParams & {
+    readonly yarn: boolean;
+    readonly hardhat: HardhatOptions;
+  };
+  
+  export type createContext = createContextOptions & {
+    readonly projectDir: string;
+    readonly scriptsDir: string;
+    readonly srcDir: string;
+    readonly testsDir: string;
+  };
+  
+  export type createResult = createContext & {
+    readonly status: CreationStatus;
+    readonly message: string;
+  };
+  
+  export type EnvVariable = readonly [name: string, type: string, value: string];
+  export type EnvVariables = readonly EnvVariable[];
+  
